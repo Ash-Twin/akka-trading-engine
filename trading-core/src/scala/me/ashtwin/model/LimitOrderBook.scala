2abad84564d37c42aaaa8ef5879c0a4f743e1669
Chenyu Liu <chenyu.liu@ascendex.io>
2023-03-08 18:38:21 +0800

    add mill intellij support

--- trading-core/src/scala/me/ashtwin/model/LimitOrderBook.scala
+++ trading-core/src/scala/me/ashtwin/model/LimitOrderBook.scala
@@ -1,25 +1,29 @@
 package me.ashtwin.model
 import scala.collection.mutable
 
-/**
- * @author Chenyu Liu
- * @since 3/8/23 Wednesday
- *
- * */
-case class LimitOrderBook(bidsBook: mutable.ArrayDeque[Order], asksBook: mutable.ArrayDeque[Order]) {
+/** @author
+ *    Chenyu Liu
+ *  @since 3/8/23
+ *    Wednesday
+ */
+case class LimitOrderBook(
+  bidsBook: mutable.ArrayDeque[Order],
+  asksBook: mutable.ArrayDeque[Order]
+) {
 
-  /**
-   * A limit order book is usually summarized by the following characteristics:
+  /** A limit order book is usually summarized by the following characteristics:
    *
-   * Bid: The highest price against which a sell order can be executed
-   * Ask: The lowest price against which a buy order can be executed
-   * Spread: The difference between the lowest ask and the highest bid
-   * Midpoint: The price halfway between the ask and the bid ((ask+bid)/2)
-   *
+   *  Bid: The highest price against which a sell order can be executed Ask: The lowest price
+   *  against which a buy order can be executed Spread: The difference between the lowest ask and
+   *  the highest bid Midpoint: The price halfway between the ask and the bid ((ask+bid)/2)
    */
-  def bids: BigDecimal = bidsBook.headOption.fold(BigDecimal(0))(_.price)
-  def asks: BigDecimal = asksBook.headOption.fold(BigDecimal(0))(_.price)
-  def spread: BigDecimal = (bids - asks).abs
+  def bids: BigDecimal     = bidsBook.headOption.fold(BigDecimal(0))(_.price)
+  def asks: BigDecimal     = asksBook.headOption.fold(BigDecimal(0))(_.price)
+  def spread: BigDecimal   = (bids - asks).abs
   def midPoint: BigDecimal = (bids + asks) / 2
 
+}
+object LimitOrderBook {
+  def empty: LimitOrderBook =
+    LimitOrderBook(mutable.ArrayDeque.empty, mutable.ArrayDeque.empty)
 }
